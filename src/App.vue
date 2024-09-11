c81ed3b63626feb4fedb7dff790791f526b0459d
muratkoyun <mkoyun@red.software.systems>
2024-09-11 10:42:08 +0200

    "composition API best practises"

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="container">
+    <h2>{{ user.name }}</h2>
+    <h3>{{ user.age }}</h3>
+  </section>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+//reactive is a bit like ref but explicilty for objects
+
+const user = ref({
+  name: 'Murat',
+  age: 30,
+});
+
+setTimeout(() => {
+  user.value.name = 'Mirko';
+  user.value.age = 24;
+}, 2000);
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+.container {
+  margin: 3rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  text-align: center;
+}
+</style>
